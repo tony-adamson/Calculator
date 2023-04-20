03142c9e5988381ff3080ee7090908b63a0b43ca
Anton <antony.adamson@gmail.com>
2023-04-20 22:53:54 +0600

    Initial Commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Антон Адамсон on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
